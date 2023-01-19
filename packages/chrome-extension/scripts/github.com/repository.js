65a866c6101293cf7f408a0681e354ed30d58726
Guillaume Bernos <guillaume@bernos.dev>
2023-01-19 13:48:45 +0100

    feat(chrome-extension): add chrome extension to preview example from github.com easily

--- packages/chrome-extension/scripts/github.com/repository.js
+++ packages/chrome-extension/scripts/github.com/repository.js
@@ -1,11 +1,16 @@
 function main() {
+  const exampleFolder = document.querySelector('[title="example"]');
   const pubspecs = document.querySelectorAll('[title="pubspec.yaml"]');
 
   if (!files) return;
 
   for (const child of pubspecs) {
     const zappButton = document.createElement("a");
-    zappButton.href = `https://zapp.run/github/${location.pathname}`;
+    let href = `https://zapp.run/github/${location.pathname}`;
+    if (exampleFolder) {
+      href += "/example";
+    }
+    zappButton.href = href;
     zappButton.target = "_blank";
     zappButton.style.marginLeft = "16px";
     zappButton.innerHTML = `<img src="https://static.invertase.io/assets/zapp/logo.svg" alt="Logo" width="16" style="margin-right: .3rem;" /><span>Open in Zapp!</span>`;

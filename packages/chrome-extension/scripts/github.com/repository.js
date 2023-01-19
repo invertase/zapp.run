1b701a8e7efc4b9d0ee14201785247bb0880849e
Guillaume Bernos <guillaume@bernos.dev>
2023-01-19 15:26:18 +0100

    feat(chrome-extension): add chrome extension to preview example from github.com easily

--- packages/chrome-extension/scripts/github.com/repository.js
+++ packages/chrome-extension/scripts/github.com/repository.js
@@ -1,21 +1,19 @@
 function main() {
   const exampleFolder = document.querySelector('[title="example"]');
-  const pubspecs = document.querySelectorAll('[title="pubspec.yaml"]');
+  const pubspecYaml = document.querySelector('[title="pubspec.yaml"]');
 
-  if (!pubspecs) return;
+  if (!pubspecYaml) return;
 
-  for (const child of pubspecs) {
-    const zappButton = document.createElement("a");
-    let href = `https://zapp.run/github/${location.pathname}`;
-    if (exampleFolder) {
-      href += "/example";
-    }
-    zappButton.href = href;
-    zappButton.target = "_blank";
-    zappButton.style.marginLeft = "16px";
-    zappButton.innerHTML = `<img src="https://static.invertase.io/assets/zapp/logo.svg" alt="Logo" width="16" style="margin-right: .3rem;" /><span>Open in Zapp!</span>`;
-    child.parentElement.appendChild(zappButton);
+  const zappButton = document.createElement("a");
+  let href = `https://zapp.run/github/${location.pathname}`;
+  if (exampleFolder) {
+    href += "/example";
   }
+  zappButton.href = href;
+  zappButton.target = "_blank";
+  zappButton.style.marginLeft = "16px";
+  zappButton.innerHTML = `<img src="https://static.invertase.io/assets/zapp/logo.svg" alt="Logo" width="16" style="margin-right: .3rem;" /><span>Open in Zapp!</span>`;
+  pubspecYaml.parentElement.appendChild(zappButton);
 }
 
 main();
